1708cbe179102ef61e5516a4b358feca56702cfa
Guilherme Diego <guidiego.expgames@gmail.com>
2017-03-24 13:21:20 -0300

    Fix: PEP8 n Flakes warnings

--- app/sell/tests/test_model.py
+++ app/sell/tests/test_model.py
@@ -12,13 +12,16 @@ def sell():
 def test_get_total_parcels_under_25(sell):
     assert sell.get_total_parcels() == 12
 
+
 def test_get_total_parcels_under_35(sell):
     sell.client.age = 26
     assert sell.get_total_parcels() == 8
 
+
 def test_get_total_parcels_over_35(sell):
     sell.client.age = 36
     assert sell.get_total_parcels() == 6
+
 
 def test_get_parcel_price(sell, mocker):
     mocked_valid = mocker.patch.object(
@@ -29,6 +32,7 @@ def test_get_parcel_price(sell, mocker):
 
     mocked_valid.return_value = 2
     assert sell.get_parcel_price() == (1000/2)
+
 
 def test_valid_sell_client_major(sell, mocker):
     mocked_valid = mocker.patch.object(
@@ -40,6 +44,7 @@ def test_valid_sell_client_major(sell, mocker):
     mocked_valid.return_value = True
     assert sell.valid_sell() is True
 
+
 def test_valid_sell_client_no_major(sell, mocker):
     mocked_valid = mocker.patch.object(
         sell.client,
@@ -49,13 +54,3 @@ def test_valid_sell_client_no_major(sell, mocker):
 
     mocked_valid.return_value = False
     assert sell.valid_sell() is False
-
-def test_get_parcel_price(sell, mocker):
-    mocked_valid = mocker.patch.object(
-        sell,
-        'get_total_parcels',
-        autospec=True,
-    )
-
-    mocked_valid.return_value = 2
-    assert sell.get_parcel_price() == (1000/2)
